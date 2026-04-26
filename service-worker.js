ddb92b85cd5eaaae3f43a97982420b1a9a747ec2
Rishab CSS <cssrishab@gmail.com>
2026-04-27 01:59:32 +0530

    ADDED PWA

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "erp-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/navbar.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
